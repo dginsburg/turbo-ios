b6f029ad16ab52835b504a6dba1d5422f264b197
Dylan Ginsburg <dylan@hey.com>
2024-01-29 14:05:59 -0500

    Make the currentVisit the topmostVisit immediately after the request finishes, instead of after it renders. This is necessary if Turbo invalidates the request when navigating back.

--- Source/Session/Session.swift
+++ Source/Session/Session.swift
@@ -152,6 +152,7 @@ extension Session: VisitDelegate {
 
     func visitRequestDidFinish(_ visit: Visit) {
         delegate?.sessionDidFinishRequest(self)
+        completeNavigationForCurrentVisit()
     }
 
     func visit(_ visit: Visit, requestDidFailWithError error: Error) {
